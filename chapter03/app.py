a7911dadec9ae7b153820464d6fd8c256efdfbe2
lucaseo <lucaseo0147@gmail.com>
2022-10-19 10:59:47 +0900

    chp03: implement simple ping

---
+++ chapter03/app.py
@@ -0,0 +1,17 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/ping")
+async def ping():
+    return "pong"
+
+
+if __name__ == "__main__":
+
+    uvicorn.run(
+        app="app:app",
+        host="127.0.0.1",
+        port=8989
+    )
