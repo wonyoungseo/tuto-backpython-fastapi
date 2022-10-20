c97b495f82e57c167052d5189ef5e084b1cbe5db
lucaseo <lucaseo0147@gmail.com>
2022-10-20 14:09:30 +0900

    update chp 3

--- chapter03/chp03_app.py
+++ chapter03/chp03_app.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI() # (1)
 
-@app.get("/ping")
+@app.get("/ping") # (2)
 async def ping():
     return "pong"
+
+# (1) FastAPI 클래스를 객체화
+# (2) 데코레이터를 통해 함수를 엔드포인트에 등록.
