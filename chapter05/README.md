a3731e2ab00870eb6e5861ec46600c4eeedd240f
lucaseo <lucaseo0147@gmail.com>
2022-10-21 16:22:14 +0900

    chp05 tweet endpoint - basic

--- chapter05/README.md
+++ chapter05/README.md
@@ -1,10 +1,16 @@
-How to run
+## basic
+
+- 기존 Flask 코드의 스타일을 최대한 유지한 상태로 FastAPI로 구현함. 
+
+### 실행
 
 ```shell
-$ uvicorn chp05_app:app
+$ uvicorn chp05_app_01_basic:app
 ```
 
-Add users
+### 회원가입
+
+- Query Parameter를 통해 전달
 
 ```shell
 $ http POST http://localhost:8989/sign_up username=kim email=kim@world.com
@@ -21,6 +27,8 @@ server: uvicorn
 }
 ```
 
+### 가입된 회원 리스트 출력
+
 ```shell
 http POST http://localhost:8989/sign_up username=den email=den@world.com
 HTTP/1.1 200 OK
@@ -36,26 +44,34 @@ server: uvicorn
 }
 ```
 
-get list of signed users
+트윗 작성
+
+- 존재하지 않는 user_idx로 작성 시, 에러 메세지 발생
 
 ```shell
-$ http GET http://localhost:8989/user_list                          
-HTTP/1.1 200 OK
-content-length: 166
+http POST http://localhost:8989/tweet user_index=99 tweet="Hello World"
+HTTP/1.1 400 Bad Request
+content-length: 28
 content-type: application/json
-date: Thu, 20 Oct 2022 05:06:52 GMT
+date: Fri, 21 Oct 2022 07:12:34 GMT
 server: uvicorn
 
 {
-    "1": {
-        "email": "kim@world.com",
-        "idx": 1,
-        "username": "kim"
-    },
-    "2": {
-        "email": "den@world.com",
-        "idx": 2,
-        "username": "den"
-    }
+    "msg": "user id not exists"
+}
+```
+
+- 트윗 작성
+
+```shell
+http POST http://localhost:8989/tweet user_index=1 tweet="Hello World"
+HTTP/1.1 200 OK
+content-length: 29
+content-type: application/json
+date: Fri, 21 Oct 2022 07:14:16 GMT
+server: uvicorn
+
+{
+    "input_tweet": "Hello World"
 }
 ```
---
+++ chapter05/chp05_app_01_basic.py
@@ -0,0 +1,85 @@
+import datetime
+from typing import List
+
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse # (1)
+from fastapi import status
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+@app.get("/ping")
+async def ping():
+    return "pong"
+
+app.APP_USERS: dict = {}
+app.USER_INDEX: int = 1
+app.TWEET: List[dict] = []
+
+
+#### Endpoint: Sign up
+
+class NewUserInput(BaseModel): # (2)
+    username: str
+    email: str
+
+
+@app.post("/sign_up") # (3)
+async def sign_up(new_user: NewUserInput):
+
+    new_user = new_user.dict()
+    new_user["idx"]=app.USER_INDEX
+    app.APP_USERS[new_user["idx"]] =new_user
+    app.USER_INDEX += 1
+
+    return JSONResponse(
+        content = new_user,
+        status_code = status.HTTP_201_CREATED
+    )
+
+
+#### Endpoint: Tweet
+
+class TweetInput(BaseModel):
+    user_index: int
+    tweet: str
+
+@app.post("/tweet")
+async def tweet(tweet_input: TweetInput):
+
+    user_idx = tweet_input.user_index
+    tweet = tweet_input.tweet
+
+    ### (5)
+    if user_idx not in app.APP_USERS:
+        return JSONResponse(
+            content={"msg": "user id not exists"},
+            status_code=status.HTTP_400_BAD_REQUEST
+        )
+
+    if len(tweet) > 100:
+        return JSONResponse(
+            content={"msg": "tweet message exceeds 100 characters"},
+            status_code=status.HTTP_400_BAD_REQUEST
+        )
+
+    app.TWEET.append(
+        {'idx': user_idx,
+         'tweet': tweet,
+         'datetime': datetime.datetime.now()}
+    )
+
+    return JSONResponse(
+        content={"input_tweet": tweet},
+        status_code=status.HTTP_200_OK
+    )
+
+
+
+# (1): JSONResponse - dict을 json으로 변환하여 http 응답으로 보낼 수 있음
+# (2): Pydantic 데이터모델 - Http request를 통해 전달되는 데이터가 사전에 정의된 형태(타입)에 부합하는지 확인할 수 있음
+# (3): 엔드포인트 sign_up - 유저명과 이메일을 입력하여 Sign up 하는 엔드포인트.
+# (4): 엔드포인트 user_list - Sign up된 리스트 GET 메소드
+# (5): 데이터 validation - tweet 엔드포인트에 전달하는 데이터의 검증을 pydantic.validator를 통해 검증.
+
